0e57db42e6c533572b960096b8887ee82b6218c4
didoshotev <didoshotev@gmail.com>
2023-05-18 14:47:45 +0300

    get by id

--- src/routes/statement.js
+++ src/routes/statement.js
@@ -12,11 +12,23 @@ router.get('/statements', async (req, res) => {
     }
 })
 
+router.get('/statements/:_id', async (req, res) => {
+    try {
+        const bankStatement = await BankStatement.findById(req.params._id)
+        if (!bankStatement) {
+            return res.status(404).json({ message: 'Bank statement not found' })
+        }
+        res.status(200).json(bankStatement)
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({ message: 'Internal server error' })
+    }
+})
+
 router.post('/statement', async (req, res) => {
     try {
         const bankStatement = new BankStatement(req.body)
-        const result = await bankStatement.save()
-        console.log('result: ', result)
+        await bankStatement.save()
         return res.status(201).send('Success')
     } catch (err) {
         console.error(err)
